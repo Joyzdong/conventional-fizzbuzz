db3544acc429b4c0950628d0d9fa8f71904362f3
Joyzdong <dwook.lee@gmail.com>
2023-09-15 15:28:41 +0900

    refactor: Do fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -9,12 +9,8 @@ def do_fizzbuzz(num:int):
     '''
     
     for i in range(1, num+1):
-        if i%15 == 0:
-            print('fizzbuzz')
-        elif i%3 == 0:
-            print('fizz')
-        elif i%5 == 0:
-            print('buzz')
+        if i%3 == 0 or i%5 ==0:
+            print('fizzbuzz' if i%15 == 0 else 'fizz' if i%3 == 0 else 'buzz')
         else:
             print(i)
 
